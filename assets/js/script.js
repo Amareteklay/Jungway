dfdc742a6de13037f808313d8c4caa8e9198c011
Amare Teklay Hailu <amareteklay@gmail.com>
2022-01-26 13:46:07 +0000

    add responsive menu icon

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,9 @@
 function myFunction() {
-    var x = document.getElementById("menu");
-    if (x.className === "menu") {
-      x.className += " responsive";
-    } else {
-      x.className = "menu";
-    }
+  var x = document.getElementById("menu");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
   }
+}
   
